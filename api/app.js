44e7e9462e0f1260182bd902bd65ec6720502821
Evandro oliveira <vandoaparecido@hotmail.com>
2021-06-06 09:20:38 -0300

    Trabalhando com rotas

--- api/app.js
+++ api/app.js
@@ -1,8 +1,31 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.use(express.json());
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/usuarios', function (req, res) {
+  res.json({
+    err:false,
+    message:"Listar usuários"
+  })
+});
+
+// Enviar no insomina
+// http://localhost:3000/login
+// {
+//   "usuario":"Evandro",
+//   "senha":"123"
+// }
+
+app.post('/login', function (req, res) {
+  //console.log(req.body);
+  console.log(req.body.usuario);
+  res.json({
+    err:false,
+    message:"Página de login"
+  })
+});
+ 
+app.listen(3000, function(){
+    console.log("Servidor iniciado na porta 3000");
 })
- 
-app.listen(3000)
